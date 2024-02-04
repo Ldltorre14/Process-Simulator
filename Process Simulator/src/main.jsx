fb9aa7ce59834bd617e0c8b60f333c8012f0215e
Ldltorre14 <leonardo.perez4292@alumnos.udg.mx>
2024-02-04 01:01:54 -0600

    FEATURES ADDED -"Container" Component for encapsulating the different elements -Base "Neon" style on Container.css and style.css

--- Process Simulator/src/main.jsx
+++ Process Simulator/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Container from './components/Container/Container'
+import "./style.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <section className='main-container'>
+    <h1 className='main-header'>Process Simulator</h1>
+    <Container className="settings-container">
+      <h2 className="settings-header">Settings</h2>
+    </Container>
+    <div className="batch-container">
+      <Container className="pending-batch-container">
+        <h2 className='pending-header'>Pending Batch</h2>
+      </Container>
+      <Container className="running-batch-container">
+        <h2 className='running-header'>Running Batch</h2>
+      </Container>
+      <Container className="finished-batch-container">
+        <h2 className='finished-header'>Finished Batch</h2>
+      </Container>
+    </div>
+  </section>
+)  
+  
+
