86040b3181ee4095d97e9e5fc41033543a2db7f6
Ldltorre14 <leonardo.perez4292@alumnos.udg.mx>
2024-02-17 01:43:31 -0600

    Total Restructure of the project -Divided in Top and Single Level Components -SettingsContainer added

---
+++ Process Simulator/src/components/Single-Level Components/submit-button/SubmitButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SubmitButton.css";
+
+function SubmitButton({text}){
+
+    return (
+        <button>
+            
+        </button>
+    );
+
+}
+
+
+export default SubmitButton;
--- Process Simulator/src/main.jsx
+++ Process Simulator/src/main.jsx
@@ -1,28 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Container from './components/Container/Container'
-import "./style.css"
+import "./index.css"
+import App from './app'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <section className='main-container'>
-    <h1 className='main-header'>Process Simulator</h1>
-    <Container className="settings-container">
-      <h2 className="settings-header">Settings</h2>
-    </Container>
-    <div className="batch-container">
-      <Container className="pending-batch-container">
-        <h2 className='pending-header'>Pending Batch</h2>
-      </Container>
-      <Container className="running-batch-container">
-        <h2 className='running-header'>Running Batch</h2>
-      </Container>
-      <Container className="finished-batch-container">
-        <h2 className='finished-header'>Finished Batch</h2>
-      </Container>
-    </div>
-  </section>
+  <>
+    <App />
+  </>
+  
 )  
   
 
